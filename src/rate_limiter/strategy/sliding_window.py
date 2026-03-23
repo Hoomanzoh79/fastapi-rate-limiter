97efd3ec0232c75d6e3f45783a0fed328ec3cf68
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2026-03-23 13:09:24 +0330

    Add SlidingWindowStrategy

---
+++ src/rate_limiter/strategy/sliding_window.py
@@ -0,0 +1,28 @@
+import time
+
+from src.rate_limiter.models import RateLimitResult
+from src.rate_limiter.store.base import RateLimitStore
+
+class SlidingWindowStrategy:
+
+    def __init__(self, store:RateLimitStore, limit: int, window: int):
+        self.store = store
+        self.limit = limit
+        self.window = window
+
+    async def check(self, key: str) -> RateLimitResult:
+        now = time.time()
+        window_start = now - self.window
+        redis_key = f"rate:{key}"
+        
+        await self.store.zremrangebyscore(redis_key, 0, window_start)
+        count = await self.store.zcard(redis_key)
+
+        if count >= self.limit:
+            return RateLimitResult(False, 0, self.window)
+
+        await self.store.zadd(redis_key, now, str(now))
+
+        remaining = self.limit - (count + 1)
+
+        return RateLimitResult(True, remaining, self.window)
