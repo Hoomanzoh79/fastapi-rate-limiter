1961ea4c1ce33dc6f5c7db1d7ad12bd4667b02e4
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2026-03-23 13:06:35 +0330

    Add RateLimitStore Protocol

---
+++ src/rate_limiter/store/base.py
@@ -0,0 +1,22 @@
+from typing import Protocol
+
+
+class RateLimitStore(Protocol):
+
+    async def incr(self, key: str, expire: int) -> int:
+        ...
+
+    async def get(self, key: str):
+        ...
+
+    async def set(self, key: str, value: int, expire: int):
+        ...
+
+    async def zadd(self, key: str, score: float, member: str):
+        ...
+
+    async def zremrangebyscore(self, key: str, min_score: float, max_score: float):
+        ...
+
+    async def zcard(self, key: str) -> int:
+        ...
