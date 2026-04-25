542e11ac1b59ceace30a941948bcfa6410ffc9e3
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2026-04-25 17:05:35 +0330

    use ABC instead of Protocol

--- src/rate_limiter/store/base.py
+++ src/rate_limiter/store/base.py
@@ -1,21 +1,27 @@
-from typing import Protocol
+from abc import ABC,abstractmethod
 
-class RateLimitStore(Protocol):
+class RateLimitStore(ABC):
 
+    @abstractmethod
     async def incr(self, key: str, expire: int) -> int:
-        ...
+        raise NotImplementedError
 
+    @abstractmethod
     async def get(self, key: str):
-        ...
+        raise NotImplementedError
 
+    @abstractmethod
     async def set(self, key: str, value: int, expire: int):
-        ...
+        raise NotImplementedError
 
+    @abstractmethod
     async def zadd(self, key: str, score: float, member: str):
-        ...
+        raise NotImplementedError
 
+    @abstractmethod
     async def zremrangebyscore(self, key: str, min_score: float, max_score: float):
-        ...
+        raise NotImplementedError
 
+    @abstractmethod
     async def zcard(self, key: str) -> int:
-        ...
+        raise NotImplementedError
--- src/rate_limiter/strategy/base.py
+++ src/rate_limiter/strategy/base.py
@@ -1,8 +1,9 @@
-from typing import Protocol
+from abc import ABC,abstractmethod
 
 from src.rate_limiter.models import RateLimitResult
 
-class RateLimitStrategy(Protocol):
+class RateLimitStrategy(ABC):
 
+    @abstractmethod
     async def check(self, key: str) -> RateLimitResult:
-        ...
+        raise NotImplementedError
